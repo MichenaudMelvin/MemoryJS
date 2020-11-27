51da4b4ef33cae5cc3b97b79c9ae4ad0f5580bc5
MichenaudMelvin <m.michenaud@rns.ecolescreatives.com>
2020-11-27 18:32:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-var risoto = document.getElementById("risoto");
-
-
-// Write JavaScript here 
-/*
 function display(){
   document.getElementById("carte").style.color = "blue";
+  document.getElementById("carte").style.backgroundColor = "grey";
   document.getElementById("carte").innerHTML = "test";
 }
 
-document.getElementById("carte").onclick = function {display()};
-*/
-
-function test(){
- risoto.style.color = "blue";
-  risoto.innerHTML = "test";
-};
-
-//fonction de test
-
-  risoto.onclick = function() {
-  test();
-};
+document.getElementById("carte").onclick = function() {display()};
