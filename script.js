0d89afe1aa7d7ed080553a6efe64f1d25f83de25
MichenaudMelvin <m.michenaud@rns.ecolescreatives.com>
2020-11-27 18:54:45 +0100

    MemoryJS

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-function display(){
-  document.getElementById("carte").style.color = "blue";
-  document.getElementById("carte").style.backgroundColor = "grey";
-  document.getElementById("carte").innerHTML = "test";
+function display0(){
+  document.getElementById("carte0").style.color = "red";
+  document.getElementById("carte0").style.backgroundColor = "grey";
+  document.getElementById("carte0").innerHTML = "test";
 }
 
-document.getElementById("carte").onclick = function() {display()};
+function display1(){
+  document.getElementById("carte1").style.color = "red";
+  document.getElementById("carte1").style.backgroundColor = "grey";
+  document.getElementById("carte1").innerHTML = "test";
+}
+
+function display2(){
+  document.getElementById("carte2").style.color = "red";
+  document.getElementById("carte2").style.backgroundColor = "grey";
+  document.getElementById("carte2").innerHTML = "test";
+}
+
+function display3(){
+  document.getElementById("carte3").style.color = "red";
+  document.getElementById("carte3").style.backgroundColor = "grey";
+  document.getElementById("carte3").innerHTML = "test";
+}
+
+function display4(){
+  document.getElementById("carte4").style.color = "red";
+  document.getElementById("carte4").style.backgroundColor = "grey";
+  document.getElementById("carte4").innerHTML = "test";
+}
+
+function display5(){
+  document.getElementById("carte5").style.color = "red";
+  document.getElementById("carte5").style.backgroundColor = "grey";
+  document.getElementById("carte5").innerHTML = "test";
+}
+
+document.getElementById("carte0").onclick = function() {display0()};
+document.getElementById("carte1").onclick = function() {display1()};
+document.getElementById("carte2").onclick = function() {display2()};
+document.getElementById("carte3").onclick = function() {display3()};
+document.getElementById("carte4").onclick = function() {display4()};
+document.getElementById("carte5").onclick = function() {display5()};
