6df4ac610d695206713af28528680dc15ecd8e32
MichenaudMelvin <m.michenaud@rns.ecolescreatives.com>
2020-11-27 18:16:36 +0100

    MemoryJS

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var risoto = document.getElementById("risoto");
+
+
+// Write JavaScript here 
+/*
+function display(){
+  document.getElementById("carte").style.color = "blue";
+  document.getElementById("carte").innerHTML = "test";
+}
+
+document.getElementById("carte").onclick = function {display()};
+*/
+
+function test(){
+ risoto.style.color = "blue";
+  risoto.innerHTML = "test";
+};
+
+//fonction de test
+
+  risoto.onclick = function() {
+  test();
+};
